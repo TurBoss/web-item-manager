b721f5b18e2f44ddc5e464ee4b6f07dfaa4fecf7
sparrow <notvita@protonmail.ch>
2017-08-20 19:05:35 +1000

    Moved clientside code into client dir

--- src/store/index.js
+++
@@ -1,7 +0,0 @@
-import { createStore, applyMiddleware } from 'redux';
-import thunk from 'redux-thunk';
-import RootReducer from '../reducers';
-
-export default ({ state = {} }) => (
-  createStore(RootReducer, state, applyMiddleware(thunk))
-);
