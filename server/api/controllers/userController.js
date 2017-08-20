def9e1a20c8ba2a242e654055a50c53abee32dfa
sparrow <notvita@protonmail.ch>
2017-08-20 18:03:32 +1000

    worked on server login controller

--- server/api/controllers/userController.js
+++ server/api/controllers/userController.js
@@ -1,21 +1,30 @@
+const bcrypt = require('bcrypt');
 const users = require('../../users.json');
 
 function userExists(user) {
   return user in users;
 }
 
-function checkPassword() {
+function checkPassword(username, password) {
+  const hashedPassword = users[username].password;
 
+  return bcrypt.compareSync(password, hashedPassword);
 }
 
 exports.login = (req, res) => {
   const { username, password } = req.body;
 
   if (!userExists(username)) {
-    return res.json.status(401).json({
-      message: `user: ${username} doesn't exist`
+    return res.status(401).json({
+      message: `User: ${username} doesn't exist`
     });
   }
 
-  res.json({ message: `login attempt.. ${username} .. ${msg}`, password });
+  if (!checkPassword(username, password)) {
+    return res.status(401).json({
+      message: 'Incorrect password'
+    });
+  }
+
+  return res.json({ message: `login attempt.. ${username} .. `, password });
 };
--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bcrypt = require('bcrypt');
 const bodyParser = require('body-parser');
 const fse = require('fs-extra');
+require('dotenv').config();
 
 const userRouter = require('./api/routes/userRoute');
 
@@ -25,7 +26,7 @@ app.use(bodyParser.json());
 
 app.use('/api/user', userRouter);
 
-app.listen(process.env.PORT || 6922, () => {
+app.listen(process.env.PORT || 55106, () => {
   /*  eslint no-console: 0  */
   console.log('Express server started');
 });
